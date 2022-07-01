cd68582e6d2bfc1f477282b0110068f75461ac12
Vasco Santos <santos.vasco10@gmail.com>
2022-07-01 09:29:36 +0200

    fix: revert wrangler build flow

--- test/scripts/utils.js
+++ test/scripts/utils.js
@@ -21,7 +21,7 @@ export function getMiniflare () {
     // We don't want to rebuild our worker for each test, we're already doing
     // it once before we run all tests in package.json, so disable it here.
     // This will override the option in wrangler.toml.
-    // buildCommand: undefined,
+    buildCommand: undefined,
     wranglerConfigEnv: 'test',
     modules: true,
     bindings: {
